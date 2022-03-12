7bb46d83c91c93039758c49a3bde5584dee02e95
cforcross <ntsendifor@gmail.com>
2022-03-13 01:16:24 +0300

    tes

---
+++ build/app/generated/source/buildConfig/release/com/example/flutter_application_1/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.flutter_application_1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.flutter_application_1";
+  public static final String BUILD_TYPE = "release";
+}
